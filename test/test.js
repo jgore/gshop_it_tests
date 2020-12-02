6a53150f8e9f8a27039f31597a08cbf3386ee8ab
goreit <p.szczepkowski87@gmail.com>
2020-12-02 10:34:09 +0100

    add automated test to add all categories and all products in each cart/order

--- test/test.js
+++ test/test.js
@@ -6,6 +6,18 @@ var expect = chai.expect;
 
 chai.use(chaiHttp);
 
+async function sendAddCartLineRequest(app, product) {
+    await chai.request(app)
+        .post("/carts/cartLine")
+        .auth('admin', 'admin')
+        .send({
+            userId: "admin",
+            productTitle: product.title,
+            quantity: getRandomInt(10),
+            sellerId: product.sellerId
+        })
+}
+
 describe('get all products by category', async () => {
 
     const app = "http://localhost:8080"
@@ -15,31 +27,28 @@ describe('get all products by category', async () => {
         expect(response.body.length).to.be.greaterThan(0)
     });
 
-    it('should create cart', async () => {
+    it('should create  cart/order  with it products from all categories ', async () => {
 
-        const response = await sendProductRequest(app, 'IT')
+        const responseCategory = await chai.request(app)
+            .get("/category")
+            .send();
 
-        for(let product of response.body) {
-            console.log(product)
-            console.log("------\n\n")
+        for( let category of responseCategory.body){
+            const response = await sendProductRequest(app, category.name)
 
-            await chai.request(app)
-                .post("/carts/cartLine")
+            for(let product of response.body) {
+                await sendAddCartLineRequest(app, product);
+            }
+
+            const confirmResponse = await chai.request(app)
+                .post("/carts/confirm")
                 .auth('admin', 'admin')
-                .send({
-                    cartId: "admin",
-                    productTitle: product.title,
-                    quantity: getRandomInt(10),
-                    sellerId:product.sellerId
-                })
+                .send()
+
+            console.log(confirmResponse.body)
         }
 
-        const confirmResponse = await chai.request(app)
-            .post("/carts/confirm")
-            .auth('admin', 'admin')
-            .send()
 
-        console.log(confirmResponse.body)
 
     })
 
